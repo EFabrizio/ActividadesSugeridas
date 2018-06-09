f38db70e15109735df1914bbe006dca3cd48c8da
EFabrizio <fabriziocastellanos@hotmail.com>
2018-06-09 14:09:23 -0600

    Add informacion de estatus en metodologias estatus

--- ActividadesSugeridasRazorPages/Pages/Eva_momentos_metodologias_estatus/Index.cshtml.cs
+++ ActividadesSugeridasRazorPages/Pages/Eva_momentos_metodologias_estatus/Index.cshtml.cs
@@ -19,9 +19,28 @@ namespace ActividadesSugeridasRazorPages.Pages.Eva_momentos_metodologias_estatus
         }
 
         public IList<eva_momentos_metodologias_estatus> eva_momentos_metodologias_estatus { get;set; }
+        public string persona;
+        public string momento;
+        public string competencia;
+        public int momentoId;
+        public short competenciaId;
+        public int personaId;
 
-        public async Task OnGetAsync()
+
+
+        public async Task OnGetAsync(short? id)
         {
+
+            var metodologia = _context.eva_momentos_metodologia.Find(id);
+            personaId = metodologia.IdPersona;
+            competenciaId = metodologia.IdCompetencia;
+            momentoId = metodologia.IdMomento;
+
+            persona = _context.rh_cat_personas.Find(personaId).Nombre;
+            competencia = _context.eva_cat_competencias.Find(competenciaId).DesCompetencia;
+            momento = metodologia.DesMomento;
+
+
             eva_momentos_metodologias_estatus = await _context.eva_momentos_metodologia_estatus
                 .Include(e => e.Cat_estatus)
                 .Include(e => e.Cat_tipos_estatus)
